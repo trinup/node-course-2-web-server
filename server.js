9087b4a9132e1c46f3825000499a88818a88c2af
Punit Tripathi <ptripathi50@gmail.com>
2017-09-02 16:13:07 +0530

    Added projects page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects',(req, res) => {
+  res.render('projects', {
+    pageTitle: 'My Projects'
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
