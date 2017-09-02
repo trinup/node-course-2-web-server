db4844853a21bf893ebe3462b3fc752c1e4ca813
Punit Tripathi <ptripathi50@gmail.com>
2017-09-02 14:49:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log =`${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));    //built in express middleware to tell express to serve contents from the provided folder
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('Hello Express!');
+  res.render('home',{
+    pageTitle: 'Home Page',
+    name: 'Punit',
+    likes: [
+      'Coding',
+      'Bodybuilding',
+      'Biking'
+    ]
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+
+app.listen(3001, () => {
+  console.log('Server is up on port 3001');
+});
